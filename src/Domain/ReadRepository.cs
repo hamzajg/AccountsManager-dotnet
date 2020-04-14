6f496900e34538e6690098c4ab4c2534cd58558f
Hamza JGUERIM <hamzajg@gmail.com>
2020-04-14 17:09:18 +0100

    Working On Domain Repositories..

---
+++ src/Domain/ReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccountsManager.Domain
+{
+    public interface ReadRepository<T> where T : class
+    {
+        Task<ICollection<T>> GetAllByAccountIdAsync(int accountId);
+
+        Task<T> GetOneByAccountIdAndIdAsync(int accountId, int id);
+    }
+}
---
+++ src/Domain/WriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AccountsManager.Domain
+{
+    public interface WriteRepository<T> where T : class
+    {
+        Task<bool> StoreAsync(int accountId, T obj);
+
+        Task<bool> UpdateAsync(int accountId, T obj);
+
+        Task<bool> DeleteAsync(T obj);
+    }
+}
